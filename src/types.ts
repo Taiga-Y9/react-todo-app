40a0eeefa4765fef366e42fdaaa3e954eaab96cc
YamazoeTaiga <182177455+Taiga-Y9@users.noreply.github.com>
2025-11-11 17:00:14 +0900

    軽微な修正3

---
+++ src/types.ts
@@ -0,0 +1,20 @@
+
+export interface Goal {
+    id: string;
+    name: string;
+    parentId: string | null;
+    childIds: string[];
+    isDone: boolean;
+    importance: number; // 1〜5 など
+    startDate: string | null; // 日付文字列
+    deadline: string | null; // 日付文字列
+    isExpanded: boolean;
+  }
+  
+  // 辞書型のマップ
+  export interface GoalMap {
+    [id: string]: Goal;
+  }
+  
+  // 並び替えのオプション
+  export type SortOption = "none" | "deadline" | "importance" | "progress";
