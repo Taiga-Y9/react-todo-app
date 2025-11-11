5b953ba8910bdf0c6ebb41b8f7454cedd8b55f94
YamazoeTaiga <182177455+Taiga-Y9@users.noreply.github.com>
2025-11-11 15:53:14 +0900

    404

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.NODE_ENV === "production" ? `/${repositoryName}/` : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        404: path.resolve(__dirname, "404.html"),
+      },
+    },
+  },
   server: {
     port: 3000,
     strictPort: false,
